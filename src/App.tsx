fffc55c5c243d148f4921cc9b07ceb1b5003b3db
Shayne Smith <shayne.m.smith@vanderbilt.edu>
2021-03-25 19:52:10 -0500

    Add typing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import QuestionCard from "./components/QuestionCard";
 // Types
 import { QuestionState, Difficulty } from "./API";
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -23,8 +23,6 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
-
-  console.log(questions);
 
   const startTrivia = async () => {
     setLoading(true);
@@ -43,11 +41,35 @@ const App = () => {
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) {
+        setScore(prev => prev + 1);
+      }
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,  // same as `answer: answer`
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
   };
 
   const nextQuestion = () => {
-
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
 
   };
 
@@ -59,7 +81,7 @@ const App = () => {
           Start
         </button>
       ) : null}
-      {!gameOver ? <p className="score">Score:</p> : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
       {loading ? <p>Loading Questions ...</p> : null}
       {!loading && !gameOver && (
         <QuestionCard 
@@ -67,13 +89,16 @@ const App = () => {
           totalQuestions={TOTAL_QUESTIONS}
           question={questions[number].question}
           answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          userAnswers={userAnswers ? userAnswers[number] : undefined}
           callback={checkAnswer}
         />
       )}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      { !gameOver && 
+        !loading && 
+        userAnswers.length === number + 1 && 
+        number !== TOTAL_QUESTIONS - 1 &&
+        <button className="next" onClick={nextQuestion}>Next Question</button>
+      }
     </div>
   );
 }
